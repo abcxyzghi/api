e3ea35b9ffdb3ffe5284cb05db283b018207714c
Your Name <nguyenhoangthanh30703@gmail.com>
2023-09-19 08:00:06 +0700

    1st up

---
+++ helloapp/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navigation from './Component/Navigation';
+import Player from './Component/Player';
+import Footer from './Component/Footer';
+function App() {
+  return (
+   <div className='App'>
+    <Navigation/>
+    <Player/>
+    <Footer/>
+   </div>
+  );
+}
+
+export default App;
---
+++ helloapp/src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+export default class footer extends Component {
+    render(){
+  return (
+    <div className='footer'>
+        <p> copyright @ 2022</p>
+    </div>
+  )
+    }
+}
